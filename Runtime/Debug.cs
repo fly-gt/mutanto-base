51e5dcd5e849e5c2eb37efa642f80ddcacbc9d42
DESKTOP-ACUVGM6\Sergey <hub.joyway.games>
2024-07-25 23:06:59 +0300

    u

--- Runtime/Debug.cs
+++ Runtime/Debug.cs
@@ -1,7 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Debug : MonoBehaviour {
-
+namespace Mutanto {
+    public class Debug : MonoBehaviour {
+        private void Start() {
+            UnityEngine.Debug.Log("Success");
+        }
+    }
 }
+
